93432b9ca2acd79928bfa6ba0e9735a099fec86a
Yamuna Rao <63515445+yrao104@users.noreply.github.com>
2021-02-26 09:29:00 -0800

    Create Path2Splines.java

---
+++ Path2Splines.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+@Autonomous(name = "Path2Splines", group = "Concept")
+public class Path2Splines extends LinearOpMode {
+    @Override
+    public void runOpMode(){
+        SampleMecanumDrive drivetrain = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(-40, 30, 0);
+        drivetrain.setPoseEstimate(startPose);
+
+        //if(path2) { //based on Vuforia
+        Trajectory traj2 = drivetrain.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(30, 35), 0)
+                .build();
+        drivetrain.followTrajectory(traj2);
+        //drop wobble goal
+        sleep(1000);
+
+        Trajectory traj3 = drivetrain.trajectoryBuilder(traj2.end())
+                .splineTo(new Vector2d(-10, 35), 0)
+                .build();
+        drivetrain.followTrajectory(traj3);
+
+        //PAUSE TO SHOOT
+        drivetrain.turn(Math.toRadians(180));
+        sleep(1000);
+        //shoot rings into goal
+
+        Trajectory traj4 = drivetrain.trajectoryBuilder(traj3.end())
+                .splineTo(new Vector2d(-48, 24), 0)
+                .build();
+        drivetrain.followTrajectory(traj4);
+        //pick up wobble goal
+        sleep(1000);
+
+        Trajectory traj5 = drivetrain.trajectoryBuilder(traj4.end())
+                .splineTo(new Vector2d(25, 35), 0)
+                .build();
+        drivetrain.followTrajectory(traj5);
+
+        Trajectory traj6 = drivetrain.trajectoryBuilder(traj5.end())
+                .splineTo(new Vector2d(30, 35), 0)
+                .build();
+        drivetrain.followTrajectory(traj6);
+
+        //drop wobble goal
+        sleep(1000);
+
+        Trajectory traj7 = drivetrain.trajectoryBuilder(traj6.end())
+                .splineTo(new Vector2d(12, 35), 0)
+                .build();
+        drivetrain.followTrajectory(traj7);
+        //}
+    }
+}
